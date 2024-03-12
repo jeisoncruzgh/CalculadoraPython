945b4e9750c6ba017c82ccdb218b2422cd3bdc7a
jeisoncruzgh <uyd72329@gmail.com>
2024-03-11 20:33:39 -0400

    E agregado la funcion de suma

---
+++ calculadoraPython/calculadora.py
@@ -0,0 +1,9 @@
+def main():
+    return
+def suma(num_1,num_2):
+    resultado = num_1 + num_2
+    print(f"Las suma de {num_1} + {num_2} = {resultado}")
+
+numero = int(input("ingrese un numero: "))
+numero2 = int(input("ingrse oto nuemero: "))
+suma(numero,numero2)
